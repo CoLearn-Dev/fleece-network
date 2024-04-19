94b47756698f8f0b56f7cf2c9447074b249ee64b
Celve <celve03@gmail.com>
2024-04-19 20:59:48 +0800

    feat: add sede for serialization

---
+++ fleece_network/sede.py
@@ -0,0 +1,24 @@
+import pickle
+from typing import Any
+
+from safetensors.torch import load, save
+from torch import Tensor
+
+
+class Sede:
+    @staticmethod
+    def save(tensors: dict[str, Tensor], metadata: dict[str, Any]) -> bytes:
+        metadata_bytes = pickle.dumps(metadata)
+        tensors_bytes = save(tensors)
+        return (
+            len(metadata_bytes).to_bytes(4, byteorder="big")
+            + metadata_bytes
+            + tensors_bytes
+        )
+
+    @staticmethod
+    def load(b: bytes) -> tuple[dict[str, Tensor], dict[str, Any]]:
+        metadata_length = int.from_bytes(b[:4], byteorder="big")
+        metadata = pickle.loads(b[4 : 4 + metadata_length])
+        tensors = load(b[4 + metadata_length :])
+        return tensors, metadata
